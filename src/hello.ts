34d5b938eb2615d7b7ceb640d85c3954e46764b4
colder <cncolder@gmail.com>
2021-05-26 13:17:25 +0800

    change to vimeo video with autoplay

--- src/hello.ts
+++ src/hello.ts
@@ -1,5 +1,5 @@
 import {openPageAndRecording} from './openPageAndRecording';
 
-const url = 'https://www.youtube.com/watch?v=V5Fsj_sCKdg';
+const url = 'https://player.vimeo.com/video/242850098?autoplay=1&background=1#t=10s';
 
 openPageAndRecording(url);
