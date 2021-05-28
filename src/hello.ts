3ddd8a0580c6a43aadd70372e5b846d81d914007
Freewind <nowindlee@gmail.com>
2021-05-28 17:02:00 +0800

    manually start, since autoplay is not reliable

--- src/hello.ts
+++ src/hello.ts
@@ -1,5 +1,5 @@
 import {openPageAndRecording} from './openPageAndRecording';
 
-const url = 'https://player.vimeo.com/video/182382689?autoplay=1&background=1#t=30s';
+const url = 'https://player.vimeo.com/video/182382689?autoplay=0&background=1#t=30s';
 
 openPageAndRecording(url);
--- src/openPageAndRecording.ts
+++ src/openPageAndRecording.ts
@@ -8,11 +8,13 @@ const ffmpeg = require('js-ffmpeg');
 const DISPLAY = 99;
 
 export async function openPageAndRecording(url: string): Promise<void> {
-  const xvfb = new Xvfb({displayNum: DISPLAY,
+  const xvfb = new Xvfb({
+    displayNum: DISPLAY,
     // if argument contains spaces, we have to split them into multiple items
     // ['-screen', '0 1024x768x16'] ---> ['-screen', '0', '1024x768x16']
     // https://stackoverflow.com/questions/10941545/nodejs-child-process-spawn-does-not-work-when-one-of-the-args-has-a-space-in-it
-    xvfb_args: ['-screen', '0', '1024x768x16']});
+    xvfb_args: ['-screen', '0', '1024x768x16']
+  });
   xvfb.startSync();
 
   const browser = await puppeteer.launch({
@@ -31,10 +33,11 @@ export async function openPageAndRecording(url: string): Promise<void> {
 
   await page.waitForTimeout(2000);
 
-  // not sure how to force the video autoplay, so I need to click on the "play" button
+  // autoplay is not reliable since browser can force disable auto-playing, I need to click on the "play" button
   // manually
-  // const playButton = await page.$('.ytp-play-button')
-  // await playButton?.click()
+  const playButton = await page.$('.play');
+  await playButton?.click()
+
 
   ffmpeg.ffmpeg([], [
     '-f', 'x11grab',
@@ -43,7 +46,8 @@ export async function openPageAndRecording(url: string): Promise<void> {
     // we have to put the `-i` after `-f`, otherwise ffmpeg will try to handle it as an invalid file,
     // and report error: ':99.0: Protocol not found, did you mean file::99.0?'
     '-i', `:${DISPLAY}.0`,
-    '-t', '30' // 30s
+    '-t', '30', // 30s
+    '-pix_fmt', 'yuv420p' // just needed for QuickTime player
   ], '/output/video.mp4', (progress: any) => {
     console.log(progress);
   }).success((json: any) => {
